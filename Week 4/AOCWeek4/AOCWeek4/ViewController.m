57c39c40190504654d112d501223d47426a03c66
James McPherson <jemcphe@fullsail.edu>
2012-09-19 19:55:46 -0500

    Created a UILabel named userPrompt… this will hold messages to the user to let them know what action to take.

--- Week 4/AOCWeek4/AOCWeek4/ViewController.m
+++ Week 4/AOCWeek4/AOCWeek4/ViewController.m
@@ -16,16 +16,20 @@
 
 - (void)viewDidLoad
 {
+    
+    self.view.backgroundColor = [UIColor colorWithRed:0 green:0.6 blue:1 alpha:1]; /*#0099ff*/
+    
     userNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 10.0f, 90.0f, 20.0f)];
     // check if userNameLabel exists
     if (userNameLabel != nil) {
         userNameLabel.text = @"Username:";
+        userNameLabel.backgroundColor = [UIColor colorWithRed:0 green:0.6 blue:1 alpha:1]; /*#0099ff*/
         //Display username Label
         [self.view addSubview:userNameLabel];
     }
     
     //display text box
-    userNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 5.0f, 200.0f, 30.0f)];
+    userNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(100.0f, 5.0f, 200.0f, 30.0f)];
     
     //check for text box existance
     if (userNameTextField != nil) {
@@ -38,10 +42,22 @@
     
     //check for button existance
     if (loginButton != nil) {
-        loginButton.frame = CGRectMake(210.0f, 40.0f, 80.0f, 25.0f);
+        loginButton.frame = CGRectMake(220.0f, 40.0f, 80.0f, 30.0f);
         [loginButton setTitle:@"Login" forState:UIControlStateNormal];
         [self.view addSubview:loginButton];
     }
+    
+    // Create a label that will display helpful information to user
+    userPrompt = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 150.0f, 320.0f, 100.0f)];
+    
+    if (userPrompt != nil) {
+        userPrompt.backgroundColor = [UIColor whiteColor];
+        userPrompt.text = @"Please Enter Username";
+        userPrompt.textAlignment = UITextAlignmentCenter;
+        [self.view addSubview:userPrompt];
+    }
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
